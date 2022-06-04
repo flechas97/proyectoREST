6372dc515980cdfee4670e76e02d67985894ecf9
rober <flechasarcon@gmail.com>
2022-06-05 01:12:42 +0200

    cambios css

--- laravel/resources/views/change.blade.php
+++ laravel/resources/views/change.blade.php
@@ -26,14 +26,58 @@
         border-radius:8px;
     }
 </style>
-<div class="row justify-content-center mt-4">
+<style>
+    .contenedor{
+        width: 100%;
+    }
+    form{
+            display:inline;
+        }
+    .celdas{
+        background-color:rgba(255, 255, 255, 0.33);
+        border-radius:8px;
+        margin:0.5%;
+        padding:5px;
+        font-size:16px;
+        font-weight:bold;
+        color:whitesmoke;
+    }
+    .tabla{
+            height:400px;
+            overflow:scroll;
+            /* border:1px solid white;
+            border-radius:8px; */
+            overflow-x: hidden;
+            overflow-x: hidden;
+        }
+        .centrado{
+        display:flex;
+        justify-content:center;
+        /* margin: 10px 0px; */
+    }
+        .cabeceras{
+        background-color:rgba(255, 255, 255, 0.33);
+        border-radius:8px;
+        margin:0.5%;
+        padding:5px;
+        font-size:16px;
+        font-weight:bold;
+        color:whitesmoke;
+    }
+    .acciones{
+        height:10px;
+        line-height:0px;
+        padding:10px 3%;
+    }
+    .centrar{
+        display:flex;
+        justify-content:center;
+    }
+    </style>
+{{-- <div class="row justify-content-center mt-4">
     <div class="col-12 tabla">
         <form action="{{route('change',[$insert->id])}}" method="post">
             @csrf
-
-
-
-
 
             <div class="row justify-content-center">
                 <div class="col-8">
@@ -85,7 +129,59 @@
         </form>
         <p class="text-light">Ultima actualizacion: {{$insert->updated_at}}</p>
     </div>
-</div>
+</div> --}}
 
+
+<div class="container">
+
+
+    <div class="row contenedor justify-content-center mt-4">
+        <div class="col-4 tarjetas redondeo ">
+            <form action="{{route('change',['id'=>$insert->id])}}" method="post">
+                @csrf
+                <div class="row justify-content-center">
+                    <div class="col-12">
+                        <h2 class="text-center">Editar Datos de Pedido</h2>
+                    </div>
+                    <div class="col-12 centrar my-2">
+                        {{-- <input type="text" name="id" class="formulario" placeholder = "Nombre"  value="{{$insert->id}}" disabled> --}}
+                        <input class="textos" name="id_user" class="formulario" type="text" value="{{$insert->id_user}}" placeholder="ID_USER">
+                        {{-- <input type="hidden" name="id" value="{{$insert->id}}"> --}}
+                    </div>
+                    <div class="col-12 centrar my-2">
+                        {{-- <input name="nombre" type="text" class="formulario" placeholder = "Nombre" value="{{$insert->nombre}}"required> --}}
+                        <input class="textos" name="total" type="number" class="formulario" value="{{$insert->total}}" step="0.01" placeholder="TOTAL" disabled>
+                    </div>
+                    <div class="col-12 centrar my-2">
+                        {{-- <input type="number" step="0.02" name="precio" class="formulario" placeholder = "Precio" value="{{$insert->precio}}" required> --}}
+                        <select name="estado" id="">
+                            <option value="0">Pendiente</option>
+                            <option value="1">Completado</option>
+                        </select>
+                    </div>
+                    {{-- <div class="col-12 centrar my-2">
+                        <input type="number" name="stock" class="formulario" placeholder = "Stock" value="{{$insert->stock}}" required>
+                    </div> --}}
+                    {{-- <div class="col-12 centrar my-2">
+                        <select name="plato">
+                            <option value="1">Primero</option>
+                            <option value="2">Segundo</option>
+                            <option value="3">Postre</option>
+                            <option value="4">Bebida</option>
+                        </select>                
+                    </div> --}}
+                    {{-- <div class="col-12 centrar my-2">
+                        <h5>Imagen</h5>
+                        <input type="file" name="image" class="formulario" placeholder = "Cantidad">
+                    </div> --}}
+                    <div class="col-12 centrar my-2">
+                        <input type="submit"class = "formulario_btn w-25" value="Guardar">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+    </div>
 @endauth
 @endsection
